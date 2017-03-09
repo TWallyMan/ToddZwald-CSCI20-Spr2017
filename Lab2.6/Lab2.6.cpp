0d64f7e4903288a2b8b7216fc5b4ea3f288e4bf0
Todd Zwald <tzwald001@student.butte.edu>
2017-03-09 23:43:33 +0000

    Lab2.6

--- Lab2.6/Lab2.6.cpp
+++ Lab2.6/Lab2.6.cpp
@@ -1,11 +1,25 @@
+// Todd Zwald
+// 3/9/16
+/*
+    This program uses the class TemperatureConverter to convert fahrenheit to
+    celsius and kelvin, celsius to fahrenheit and kelvin, and kelvin to celsius
+    and fahrenheit. The final function outputs all o fhte information that was 
+    used in the main and will show each temepature after it has been converted
+*/
 #include<iostream>
 using namespace std;
+
+/*
+    This class holds all of the temperature conversion functions, as well as
+    their respective parameters to allow the main to input arguments. There are
+    also all of the necessary accessors and mutators for each conversion
+*/
 
 class TemperatureConverter{
     
     private:
     
-        double kelvin_;
+        double kelvin_; // variable that holds kelvin temperature for overall use
     
     public:
         
@@ -16,38 +30,41 @@ class TemperatureConverter{
             kelvin_ = tempKelvin;
         }
         
-        void SetTempFromCelsius(double tempCelsius){
+        void SetTempFromCelsius(double tempCelsius){ //converts celsius to kelvin
             kelvin_ =  tempCelsius + 273.15;
             return;
         }
-        double GetTempAsCelsius(){
+        double GetTempAsCelsius(){ // Gets temperature as celsius from kelvin
             double tempCelsius;
             tempCelsius = kelvin_ - 273.15;
             return tempCelsius;
         }
         
+        // converts fahrenheit to kelvin
         void SetTempFromFahrenheit(double tempFahrenheit){
             kelvin_ = (5 * (tempFahrenheit - 32 ) / 9) + 273.15;
             return;
         }
-        double GetTempAsFahrenheit(){
+        double GetTempAsFahrenheit(){ //gets temperature as fahrenheit from kelvin
             double tempFahrenheit;
             tempFahrenheit = (((kelvin_ - 273.15) * 9) / 5 + 32);
             return tempFahrenheit;
         }  
         
+        // keeps kelvin temperature in kelvins
         void SetTempFromKelvin(double kelvinTempSet){
             kelvin_ = kelvinTempSet;
             return;
         }
-        double GetTempFromKelvin(){
+        double GetTempFromKelvin(){ // returns kelvin temperature
             return kelvin_;
         }
         
+        // prints all values when value has input from main
         void PrintTemperatures(){
-            cout << GetTempFromKelvin() << "K" << endl;
-            cout << GetTempAsCelsius() << "C" << endl;
-            cout << GetTempAsFahrenheit() << "F" << endl;
+            cout << GetTempFromKelvin() << " Kelvin" << endl;
+            cout << GetTempAsCelsius() << " Celsius" << endl;
+            cout << GetTempAsFahrenheit() << " Fahrenheit" << endl << endl;
         }
     
 };
