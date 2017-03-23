a759e0af92301efe3f4fe8c44dabe3210610212e
Todd Zwald <tzwald001@student.butte.edu>
2017-03-23 23:33:21 +0000

    Lab3.1

---
+++ Lab3.1/Lab3.1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+if(deviceCharge == dumbphone){
+    cost = 0;
+}
+else if(deviceCharge == smartphone){
+    cost = 5;
+}
+else{
+    cost = 10;
+}
+
+
+
+int main(){
+    const int dumbPhone = 0;
+    const int smartPHone = 5;
+    const int tablet = 10;
+    const int monthlyCharge = 30;
+    const double corporateDiscount = .9;
+    const int planDiscount = -3;
+    const double dataoverCahrge = 1.5
+    const startingData = 10;
+    int additionalData;
+    double totalData = startingData + (additionalData * dataoverCahrge);
+    
+}
