398e7b35bdba5d83a418a068d9bddc652fc150e5
Todd Zwald <tzwald001@student.butte.edu>
2017-05-07 07:02:15 +0000

    Lab4.2

--- Lab4.2/Lab4.2.cpp
+++ Lab4.2/Lab4.2.cpp
@@ -1,51 +1,94 @@
 #include <iostream>
-
+#include <string>
+#include <ios>
+#include <iomanip>
 using namespace std;
 
 int main(){
-    
-    int Usernumber[11];
-    int userChoice;
-    string mainMenu[11];
-    int yesNo;
-    
-    
-    
+    const int ARRAY_SIZE = 11;
+    int Usernumber[ARRAY_SIZE];
+    string menu[ARRAY_SIZE];
+    int storage[ARRAY_SIZE];
+    double cost[ARRAY_SIZE];
+    int ordered[ARRAY_SIZE];
     int i = 0;
-    int j;
+    double orderTotal = 0;
     
-    mainMenu[0] = "New York Steak";
-    mainMenu[1] = "Salmon";
-    mainMenu[2] = "Tri-Tip";
-    mainMenu[3] = "Salad";
-    mainMenu[4] = "Soup";
-    mainMenu[5] = "Bread Sticks";
-    mainMenu[6] = "Mozzerella Sticks";
-    mainMenu[7] = "Jalapeño Poppers";
-    mainMenu[8] = "Ice Cream" ;
-    mainMenu[9] = "Chocolate Cake";
-    mainMenu[10] = "Water";
+    menu[0] = "New York Steak";
+    menu[1] = "Salmon";
+    menu[2] = "Tri-Tip";
+    menu[3] = "Salad";
+    menu[4] = "Soup";
+    menu[5] = "Bread Sticks";
+    menu[6] = "Mozzerella Sticks";
+    menu[7] = "Jalapeño Poppers";
+    menu[8] = "Ice Cream" ;
+    menu[9] = "Chocolate Cake";
+    menu[10] = "Water";
+    
+    storage[0] = 5;
+    storage[1] = 8;
+    storage[2] = 3;
+    storage[3] = 10;
+    storage[4] = 10;
+    storage[5] = 10;
+    storage[6] = 10;
+    storage[7] = 10;
+    storage[8] = 8;
+    storage[9] = 5;
+    storage[10] = 99;
+    
+    cost[0] = 10.0;
+    cost[1] = 13.50;
+    cost[2] = 18.5;
+    cost[3] = 5.0;
+    cost[4] = 5.0;
+    cost[5] = 3.0;
+    cost[6] = 4.0;
+    cost[7] = 4.5;
+    cost[8] = 5.0;
+    cost[9] = 7.0;
+    cost[10] = 1.0;
+    
+    ordered[0] = 0;
+    ordered[1] = 0;
+    ordered[2] = 0;
+    ordered[3] = 0;
+    ordered[4] = 0;
+    ordered[5] = 0;
+    ordered[6] = 0;
+    ordered[7] = 0;
+    ordered[8] = 0;
+    ordered[9] = 0;
+    ordered[10] = 0;
     
     
-    cout << "Pick any number of items from the list below:" << endl;
-    for(int i = 0; i < 11; i++){
-        cout << "#" << i + 1 << " " << mainMenu[i] << endl;
+     for(int i = 0; i < ARRAY_SIZE; i++){
+        cout << "#" << i + 1 << " " << menu[i] << "  $" << cost[i] << endl;
     }
-    
-    for(int j = 1; j < 12; j++){
-        cout <<endl;
-        cout << "I would like a #";
-        cin >> j;
-        cout << endl;
+    cout << "How many of each product would you like?" << endl << endl;
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        int qauntityWant;
+        cout << "#" << i + 1 << ": ";
+        cin >> qauntityWant;
+        if(qauntityWant > storage[i]){
+            cout << "Sorry, we dont have that many, we only have " << storage[i] << endl;
+            cin >> qauntityWant;
+        }
+        else{
+            ordered[i] = qauntityWant;
+        }
     }
+    cout << "Ok, your total items ordered will be: " << endl << endl;
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        cout << menu[i] << ":  " << ordered[i] << endl;
+    }
+    for(int i = 0; i < ARRAY_SIZE; i++){
+        double addingOrder = ordered[i] * cost[i];
+        orderTotal = addingOrder;
+    }
+    cout << "Your total bill will be: " << orderTotal;
     
-    
-    /*cout << "Is that all for you?" << endl;
-    cin >> userAnswer;
-    cout << endl;
-    
-    cout << "Ok, I'll have that ready for you soon!" << endl;
-    */
     return 0;
     
 }
